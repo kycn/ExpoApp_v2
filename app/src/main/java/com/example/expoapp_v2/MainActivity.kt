cee1c087c5462fb4e8eb40e4e511e1554a34bb98
Kayacan Kaya <35106533+kycn@users.noreply.github.com>
2022-07-26 00:45:45 +0200

    Edit git credentials

--- app/src/main/java/com/example/expoapp_v2/MainActivity.kt
+++ app/src/main/java/com/example/expoapp_v2/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,14 +30,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting() {
+    Text(text = "Hello")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ExpoApp_v2Theme {
-        Greeting("Android")
+        Greeting()
     }
 }
